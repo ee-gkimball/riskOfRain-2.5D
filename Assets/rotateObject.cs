7da6913fa0c73862e5843f4ed30934c1f4f9c7bf
Garrett Kimball <gkimball@etcedu.com>
2014-04-21 16:17:32 -0400

    Initial commit

---
+++ Assets/rotateObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateObject : MonoBehaviour {
+
+	public float speed;
+	public bool enabled;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (enabled)
+			transform.Rotate(0, Time.deltaTime * speed, 0);
+	}
+}
